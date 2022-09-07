062cc55ef7b95da3e25313f5c37ad9b549cc5ff3
igorlamoia <igorlamoia@hotmail.com>
2022-09-07 14:35:29 -0300

    feat: category card

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import './App.css';
 import { ServiceCard, WorkerCard } from './components';
 import iServiceLottie from './assets/iservice-lottie.json';
 import LottieAnimacao from 'lottie-react';
+import { Home } from './screens';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <WorkerCard /> */}
-			{/* <ServiceCard /> */}
-			<lottie-interactive path="https://assets2.lottiefiles.com/packages/lf20_i9mxcD.json" interaction="hover" />
-			<LottieAnimacao animationData={iServiceLottie} />
+			<Home />
+			{/* <WorkerCard />
+			<ServiceCard /> */}
+			{/* <lottie-interactive path={LottieAnimacao} interaction="hover" /> */}
+			{/* <LottieAnimacao animationData={iServiceLottie} /> */}
 		</div>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './reset.css';
 import './index.css';
+import { Home } from './screens';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
