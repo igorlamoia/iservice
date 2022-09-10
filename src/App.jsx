bc44ef9abe0d2d34fbf3dd7d16b2e24b3332c766
humbertomottadacunha@gmail.com <humbertomottadacunha@gmail.com>
2022-09-10 18:03:49 -0300

    Lottie

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React, { useRef } from 'react';
-import { ServiceCard, WorkerCard, SearchInput, Footer } from './components';
-import iServiceLottie from './assets/iservice-lottie.json';
-import LottieAnimacao from 'lottie-react';
-import { Home } from './pages';
+import React, { useRef } from "react";
+import { ServiceCard, WorkerCard, SearchInput, Footer } from "./components";
+import iServiceLottie from "./assets/iservice-lottie.json";
+import LottieAnimacao from "lottie-react";
+import { Home } from "./pages";
 
 function App() {
-	return (
-		<div style={{height: '300vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center'}}>
-			<SearchInput />
-			<WorkerCard />
-			<ServiceCard />
-			{/* <lottie-interactive path={LottieAnimacao} interaction="hover" /> */}
-			{/* <LottieAnimacao animationData={iServiceLottie} /> */}
-			<Footer />
-		</div>
-	);
+  return (
+    <div
+      style={{
+        height: "300vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <SearchInput />
+      <WorkerCard />
+      <ServiceCard />
+      {/* <lottie-interactive path={LottieAnimacao} interaction="hover" /> */}
+      {/* <LottieAnimacao animationData={iServiceLottie} /> */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
