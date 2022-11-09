f03d454d433c1e87aa6982c6dd0efbb04cedc337
humbertomottadacunha@gmail.com <humbertomottadacunha@gmail.com>
2022-11-09 13:45:57 -0300

    Termino loading

--- src/components/skeletons/worker-card/index.jsx
+++ src/components/skeletons/worker-card/index.jsx
@@ -11,8 +11,9 @@ export default function SkeletonWorkercard() {
           overflow: 'hidden',
           borderRadius: 1.5,
           scrollSnapAlign: 'start',
-          width: 270,
-          minWidth: 250,
+          width: 252,
+          minWidth: 252,
+          height: 473.04,
         }}
         className="card-body"
         elevation={4}
@@ -20,11 +21,20 @@ export default function SkeletonWorkercard() {
         <Stack direction="row">
           <Skeleton variant="circular" width={120} height={120} />
           <Stack sx={{ mt: 2, ml: 2 }}>
-            <Skeleton height={18} width="4.5rem" />
+            <Skeleton height={25} width="4.8rem" />
+            <Skeleton height={20} width="5rem" sx={{ borderRadius: 2 }} />
+            <Stack direction="row" alignItems="center">
+              <Skeleton variant="circular" width={20} height={20} />
+              <Skeleton height={24} width="4rem" sx={{ ml: 0.5 }} />
+            </Stack>
+            <Skeleton height={25} width="6rem" />
+            <Skeleton height={25} width="6rem" sx={{ borderRadius: 1.2 }} />
+
+            {/* <Skeleton height={18} width="4.5rem" />
             <Skeleton height={19} width="5.5rem" sx={{ ml: 0.5 }} />
             <Skeleton height={18} width="9rem" />
             <Skeleton height={18} width="9rem" />
-            <Skeleton height={18} width="9rem" />
+            <Skeleton height={18} width="9rem" /> */}
           </Stack>
         </Stack>
         <Stack
@@ -32,27 +42,40 @@ export default function SkeletonWorkercard() {
           spacing={0.7}
           justifyContent="center"
           sx={{
-            mt: 2,
+            mt: 1,
           }}
         >
-          <Skeleton variant="circular" width={24} height={24} />
-          <Skeleton variant="circular" width={24} height={24} />
-          <Skeleton variant="circular" width={24} height={24} />
-          <Skeleton variant="circular" width={24} height={24} />
-          <Skeleton variant="circular" width={24} height={24} />
-          <Skeleton variant="circular" width={24} height={24} />
-          <Skeleton variant="circular" width={24} height={24} />
+          <Skeleton variant="circular" width={26} height={26} />
+          <Skeleton variant="circular" width={26} height={26} />
+          <Skeleton variant="circular" width={26} height={26} />
+          <Skeleton variant="circular" width={26} height={26} />
+          <Skeleton variant="circular" width={26} height={26} />
+          <Skeleton variant="circular" width={26} height={26} />
+          <Skeleton variant="circular" width={26} height={26} />
         </Stack>
-        <Skeleton width={260} height={24} />
-        <Skeleton width={200} height={24} />
-        <Skeleton height={50} />
+        <Stack sx={{ mt: 0.8 }}>
+          <Skeleton width={165.85} height={24} />
+          <Stack direction="row">
+            <Skeleton width={71.91} height={18} sx={{ borderRadius: 2 }} />
+            <Skeleton
+              width={71.91}
+              height={18}
+              sx={{ borderRadius: 2, ml: 1 }}
+            />
+          </Stack>
+        </Stack>
+        <Skeleton width={164} height={21.22} sx={{ mt: 0.8 }} />
+        <Skeleton width={164} height={21.22} />
         <Stack direction="row" justifyContent="flex-end">
-          <Skeleton variant="circular" height={42} width={42} />
+          <Skeleton variant="circular" height={42.38} width={42.38} />
         </Stack>
-        <Skeleton width={200} />
-        <Skeleton height={50} />
-        <Stack alignItems="center">
-          <Skeleton width={252} height={60} sx={{ mt: 2 }} />
+        <Skeleton width={160} height={24} sx={{ mt: 1.2 }} />
+        <Stack sx={{ mt: 1 }}>
+          <Skeleton width={240} height={21.22} />
+          <Skeleton width={240} height={21.22} />
+        </Stack>
+        <Stack alignItems="center" justifyContent="center">
+          <Skeleton width={252} height={70} sx={{ mt: 2, borderRadius: 2.5 }} />
         </Stack>
       </Paper>
     </div>
